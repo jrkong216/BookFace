54ff0a6e8946f2d8991027e7c1df4b4954f18c59
Jason Kong <jrkong216@gmail.com>
2022-11-22 20:21:21 -0800

    created store post file and need to test with dispatching

---
+++ react-app/src/store/posts.js
@@ -0,0 +1,212 @@
+// store > posts.js
+
+import {
+    csrfFetch
+} from "./csrf"
+// *****************************************************************************
+//****************************** ACTION CREATORS *******************************
+
+// CRUD:
+// Create a Post
+// GET a Post
+// Update/Edit a Post
+// Delete a Post
+
+///*************************************************************************** */
+const GET_ALLPOSTS = 'posts/getAllPosts'
+const GET_ONEPOST = 'posts/getOnePost'
+const CREATE_POST = 'posts/createPost'
+const UPDATE_POST = 'posts/updatePost'
+const DELETE_POST = 'posts/removePost'
+
+///*************************************************************************** */
+// **** GET ALL CODERS ****
+const getAllPosts = posts => ({
+    type: GET_ALLPOSTS,
+    payload: posts
+})
+///*************************************************************************** */
+// **** GET ONE CODER DETAILS ****
+const getOnePost = post => ({
+    type: GET_ONEPOST,
+    payload: post
+})
+
+///*************************************************************************** */
+// **** CREATE A CODER ****
+
+const createPost = post => ({
+    type: CREATE_POST,
+    payload: post
+})
+///*************************************************************************** */
+// **** EDIT/UPDATE A CODER ****
+
+const updatePost = post => ({
+    type: UPDATE_POST,
+    payload: post
+})
+///*************************************************************************** */
+// **** DELETE A CODER ****
+
+const removePost = postId => ({
+    type: DELETE_POST,
+    payload: postId
+})
+
+// *****************************************************************************
+//************************************ THUNKS **********************************
+
+// -------------------------  LOAD ALL CODERS   ----------------------------------
+export const loadAllPosts = () => async dispatch => {
+    const response = await csrfFetch('/api/posts/')
+    if (response.ok) {
+        const postsList = await response.json();
+        // console.log("this is coders list", codersList)
+        dispatch(getAllPosts(postsList))
+    }
+}
+
+//*************************************************************************** */
+
+// -------------------------  LOAD ONE POST's DETAILS   -------------------------
+
+
+export const loadOnePost = (postId) => async dispatch => {
+    const response = await csrfFetch(`/api/posts/${postId}/`);
+    // console.log("DID TI REACH GET ONE POST THUNK")
+
+
+
+
+    if (response.ok){
+        const postInfo = await response.json();
+        //  console.log("POST INFO IN THUNK", postInfo)
+        dispatch(getOnePost(postInfo))
+    }
+}
+
+
+//*************************************************************************** */
+
+// -------------------------  CREATE A POST   ----------------------------------
+
+export const createNewPost = (payload) => async dispatch => {
+    // console.log("did this reach?")
+    // console.log("this is the payload", payload)
+    const response = await csrfFetch('/api/posts/new/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(payload)
+    })
+    // console.log("did it reach here? after response?")
+
+    if (response.ok) {
+        let post = await response.json()
+        // console.log("this is the coder if response.ok", post)
+        dispatch(createPost(post))
+        return post
+    }
+}
+
+//*************************************************************************** */
+
+// -------------------------  EDIT A CODER    ----------------------------------
+
+export const editPost = (editPostInfo) => async dispatch => {
+
+    const response = await csrfFetch(`/api/posts/${editPostInfo.id}/`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(editPostInfo)
+    })
+
+    if (response.ok) {
+        const editedPost = await response.json();
+        dispatch(updatePost(editedPost))
+        return editedPost
+    }
+}
+
+//*************************************************************************** */
+
+// -------------------------  DELETE A CODER    --------------------------------
+export const deletePost = (payload) => async dispatch => {
+    const response = await csrfFetch(`/api/posts/${payload.id}/`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    if (response.ok) {
+        dispatch(removePost(payload.id))
+        return response
+    }
+}
+
+
+
+
+// *****************************************************************************
+// ******************************* REDUCERS ************************************
+
+const initialState = {}
+
+const postReducer = (state = initialState, action) => {
+
+    let newState;
+    // *****************************************************************************
+    switch (action.type) {
+        case GET_ALLPOSTS:
+            newState = {
+                ...state
+            }
+            action.payload.Posts.forEach((post) => {
+                newState[post.id] = post
+            });
+            return newState
+            // *****************************************************************************
+            case GET_ONEPOST:
+                // newState = {}
+
+                // newState[action.payload.id] = action.payload
+
+                return { ...state, ...action.payload}
+
+            // *****************************************************************************
+        case CREATE_POST:
+            newState = {
+                ...state
+            }
+            newState[action.payload.id] = action.payload
+            return newState
+            // *****************************************************************************
+        case UPDATE_POST:
+            newState = {
+                ...state
+            }
+            newState[action.payload.id] = action.payload
+
+            return newState;
+
+
+            // *****************************************************************************
+        case DELETE_POST:
+            newState = {
+                ...state
+            }
+            delete newState[action.payload]
+            return newState
+            // *****************************************************************************
+        default:
+            return state
+
+    }
+}
+// *****************************************************************************
+
+export default postReducer
