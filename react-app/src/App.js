ada81bd901d69b0bc30800c00104eac4129e691a
Jason Kong <jrkong216@gmail.com>
2022-11-27 17:16:12 -0800

    created footer

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -28,6 +28,7 @@ function App() {
     <>
     {sessionUser ? <Navigation isLoaded={isLoaded} />: null}
       {isLoaded && (
+        <div>
         <Switch>
           <Route exact path="/">
             <HomePage/>
@@ -41,10 +42,26 @@ function App() {
           <Route path="/test">
             <CreateAPost/>
           </Route>
-
         </Switch>
+
+     <footer className="footer">
+      <div className="footer-about">
+      <strong>BookFace -  Inspired by FaceBook</strong>
+     </div>
+      <div className="footer-links">
+       <div>
+         <a id="github" className="links-github" href="https://github.com/jrkong216">Created By: Jason Kong &nbsp;
+          <i className="fa-brands fa-github fa-xl"></i>
+        </a>
+         <a id="linkedin" className="links-linkedin" href="https://www.linkedin.com/in/jason-kong-39552922/">
+        <i className="fa-brands fa-linkedin fa-xl"></i>
+      </a>
+     </div>
+    </div>
+    </footer>
+</div>
       )}
-    </>
+</>
   );
 }
 export default App;
