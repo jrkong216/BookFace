3d2662f58bbc9c6122828c0715270c6d88535fbc
Jason Kong <jrkong216@gmail.com>
2023-01-25 11:27:06 -0800

    additional work on modal comment popup

--- react-app/src/components/OpenCommentModal/OpenComment.js
+++ react-app/src/components/OpenCommentModal/OpenComment.js
@@ -6,12 +6,7 @@ import {loadAllPosts} from "../../store/posts"
 import "./OpenComment.css"
 function OpenComment({spot, commentBySpotId, deleteCommentHandler, post, commentByPostId}) {
     const dispatch = useDispatch()
-    // const [review, setComment] = useState("");
-    // const [stars, setStars] = useState(1)
-// console.log("is this showing up? comment form")
-// console.log("this is spot", spot)
-// let spotId = spot.id
-// console.log("this is spotID in OpenCOmment", spotId)
+
 
 useEffect(() => {
     dispatch(loadAllPosts(post.id))
@@ -40,93 +35,27 @@ useEffect(() => {
 
     return(
 
-        <div className="comment-outer-container">
-            <div className="left-container">
-                <div className="spot-image-container">
-                    <img className="spot-image" src={post && post.img_url} />
+        <div className="modal-comment-outer-container">
+            <div className="modal-left-container">
+                <div className="modal-spot-image-container">
+                <div className="spot-card-profile-circle-container">
+            <i className="fa fa-user-circle fa-2x" aria-hidden="true"></i>
+          </div>
+          <div className="UserName">
+            {post.users && post.users.first_name} {post.users && post.users.last_name}
+
+          </div>
+
+                <div className="description">{post.description}</div>
+                {post.img_url === ""? null :<div className="spot-image-container">
+          <img className="modal-spot-image" src={post.img_url} alt="to be seen"
+                onError={e => { e.currentTarget.src = "https://i.stack.imgur.com/6M513.png"}}/>
+        </div>}
+
                 </div>
             </div>
 
-            <div className="right-container">
-            {/* <div className="spot-link-container" to={`/spots/${spot.id}`}> */}
-            <div className="spot-link-container">
-      <div className="spot-container">
-        <div className="top-spot-container">
-          <div className="spot-card-profile-circle-container">
-            <i className="fa fa-user-circle fa-2x" aria-hidden="true"></i>
-          </div>
-          {/* <div className="UserName">
-            {spot.ownerId} thisisOwnerID Needs to be UserName[re-do backend]
-          </div> */}
-        </div>
-
-        <div className="font-awesome-container">
-            <div className="heart-container">
-            <i class="fa-regular fa-heart fa-lg"></i>
-            </div>
-            <div className="comment-container" >
-            <i class="fa-regular fa-comment fa-lg"></i>
-            </div>
-        </div>
-        <div className="likes-container">
-            <div className="likes">Likes go here</div>
-        </div>
-
-        <div className="userName-description-container">
-            <div className="userName-container">
-            {/* <div className="userName">{spot.ownerId} this needs to be userNAME</div> */}
-            </div>
-            <div className="userDescription-container">
-            {/* <div className="description-container">{spot.description}</div> */}
-            </div>
-        </div>
-
-<div className="new-comments-container">
-{commentByPostId.map((item) => {
-                        return (
-                            <div className= "comment-box-container" key={item.id}>
-
-                                <div className="spot-review-name"> { item.User ? item.User.firstName : null}:</div>
-                                {/* <div className="spot-review-name"> { item.User && item.User.firstName}:</div> */}
-                                <div className= "item-comment-container">
-                                <div className="item-comment"> {item.review}</div>
-                                </div>
-                                <div className="comment-delete-button-container">
-                                <button className="Comment-Delete-Button" onClick= {() => deleteCommentHandler(item.id, item.userId)}>Delete Comment</button>
-                                </div>
-                                {/* <EditCommentModal item={item} spotId={spotId} /> */}
-                                <EditCommentModal/>
-                            </div>
-
-                        )})
-                    }
-
-</div>
-
-        <div className="make-a-comment-container">
-            <div className="emoji-container">
-            <i class="fa-regular fa-face-smile fa-lg"></i>
-            </div>
-            <div className="actual-comment-container">
-            <form className="form-post-container" onSubmit={postHandler}>
-            {/* <label>
-                <input
-                    className="spot-card-form-inputs"
-                    type="text"
-                    value={review}
-                    onChange={(e) => setComment(e.target.value)}
-                    required
-                    placeholder="Comment Here"
-                />
-            </label> */}
-            <div className="post-button-container">
-                <button className="post-text" type="submit">Post</button>
-            </div>
-            </form>
-            </div>
-        </div>
-      </div>
-    </div>
+            <div className="modal-right-container">
 
             </div>
 
